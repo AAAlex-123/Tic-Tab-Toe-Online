9f2fe0014a336b90e5a952bfeceb50a2bc8f0374
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-09-02 12:29:59 +0300

    Update GameEngine.java

--- GameEngine.java
+++ GameEngine.java
@@ -9,10 +9,27 @@ public class GameEngine {
 		ui.setSize(400,550);
 		ui.setVisible(true);
 	  	ui.setResizable(false);
+	  	//start game
+	  	
+	  	
+	  	//this will have to be done for all UIs
+	  	String move = null;
+	  	while (move==null) {
+	  		move = ui.getAnswer();
+	  		if (move == null) {
+	  			try {
+	  				Thread.sleep(250); //wait 1/4 sec before checking 
+	  			}catch (InterruptedException e){
+	  				e.printStackTrace();
+	  			}
+	  		}
+	  	}
+	  	System.out.print(move);
+	  	if(move.equals("Resign")) {
+	  		System.out.println("Game over");
+	  		System.exit(0);
+	  	}
 	}
 	
-	public static void turnReceived(String game_obj) {
-		//do stuff
-		System.out.println(game_obj);
-	}
+	
 }
