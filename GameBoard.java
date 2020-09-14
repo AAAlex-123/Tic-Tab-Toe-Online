15c1a21fc0ef70411699b9e32e1bdd3541b38338
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-09-14 19:11:34 +0300

    making online gameboard
    idk what i have changed, if you have no clue comment

--- GameBoard.java
+++ GameBoard.java
@@ -1,62 +1,74 @@
-public class GameBoard {
+package ttt_online;
+
+import java.io.Serializable;
+
+public class GameBoard implements Serializable {
+	public static void main(String[] args) {
+		GameBoard gb = new GameBoard();
+		gb.markSquare(1, 'X');
+		System.out.println(gb.toString());
+		gb.markSquare(2, 'O');
+		System.out.println(gb.toString());
+		gb.markSquare(14, 'X');
+		System.out.println(gb.toString());
+		gb.markSquare(42, 'O');
+		System.out.println(gb.toString());
+		gb.markSquare(43, 'X');
+		System.out.println(gb.toString());
+	}
 	
 	private char[][] board = new char[5][5];
 	
 	public GameBoard() {
-		for(int i=0;i<5;i++) {
-			for(int j=0;j<5;j++) {
+		for(int i=0; i<5; i++)
+			for(int j=0; j<5; j++)
 				board[i][j] = GameEngine.DASH;
-			}
-		}
 	}
 	
-	public boolean markSquare(int coord,char mark){
-		//true if successfull, false if square already marked
-		if (board[coord/10][coord%10]==GameEngine.X||board[coord/10][coord%10]==GameEngine.O) return false;
-		else if(coord>55) return false;
-		else{
-			board[coord/10][coord%10] = mark;
-			return true;		
-		}
+	public boolean markSquare(int coord, char mark){
+		//true if successful, false if square already marked
+		if (board[coord/10][coord%10] != GameEngine.DASH || coord > 55)
+			return false;
+		board[coord/10][coord%10] = mark;
+		return true;
 	}
+
 	public boolean hasWon() {
-		return (diagWin()||horizWin(board)||vertWin(board));
+		return (diagonalWin() || horizontalWin(board) || verticalWin(board));
 	}
 	
-	private boolean horizWin(char [][] table) {
-		
-		for(char[] row: table) {
+	private boolean horizontalWin(char[][] table) {
+		for (char[] row: table) {
 			short consec = 1;
 			char curr = row[0];
 			
-			for(char c : row) {
-				if(c==curr && curr!=GameEngine.DASH) consec++;
-				else{
+			for (char c : row) {
+				if (c == curr && curr != GameEngine.DASH) consec++;
+				else {
 					curr = c;
 					consec = 1;
 				}
-				if (consec>=3) return true;
-			}//row
+				if (consec >= 3) return true;
+			}	//row
 		}
 		return false;
 	}
 	
 	private char[][] invertTable(char[][] table){
 		char[][] newTable = new char[5][5];
-		for(int i=0;i<newTable.length;i++) {
-			for(int j=0;j<newTable.length;j++) {
+		
+		for(int i=0;i<newTable.length;i++)
+			for(int j=0;j<newTable.length;j++)
 				newTable[i][j] = table[j][i];
-			}
-		}
 		return newTable;
 	}
 	
-	private boolean vertWin(char[][] table) {
-		return horizWin(invertTable(board));
+	private boolean verticalWin(char[][] table) {
+		return horizontalWin(invertTable(table));
 	}
 	
-	private boolean diagWin() {
-		int[][] directions = { {1,-1}, {1,1}};//{1,0}, {0,1} to replace all other checks as well
+	private boolean diagonalWin() {
+		int[][] directions = {{1, -1}, {1, 1}};		//{1,0}, {0,1} to replace all other checks as well
 	    for (int[] d : directions) {
 	        int dx = d[0];
 	        int dy = d[1];
@@ -66,7 +78,8 @@ public class GameBoard {
 	                int lasty = y + 2*dy;
 	                if (0 <= lastx && lastx < board.length && 0 <= lasty && lasty < board.length) {
 	                    char w = board[x][y];
-	                    if (w != GameEngine.DASH && w == board[x+dx][y+dy] && w == board[lastx][lasty])  return true;
+	                    if (w != GameEngine.DASH && w == board[x+dx][y+dy] && w == board[lastx][lasty])
+	                    	return true;	// winner
 	                }
 	            }
 	        }
