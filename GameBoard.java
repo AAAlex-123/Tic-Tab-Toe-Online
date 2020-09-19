810351005569dcd680ac7ffa130a22275f5a4bb1
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-09-19 15:36:34 +0300

    Update GameBoard.java
    cleaned up code

--- GameBoard.java
+++ GameBoard.java
@@ -20,7 +20,7 @@ public class GameBoard {
 	}
 	
 	/**
-	 * Initialise board by coping board <code>table</code>
+	 * Initialise board by copying board <code>table</code>
 	 * @param table char[][], the table to be copied
 	 */
 	public GameBoard(char[][] table) {
@@ -35,7 +35,7 @@ public class GameBoard {
 	 * 
 	 * @param coord int, the coordinate of the square to be marked, in the form of 10*row + col
 	 * @param mark char, the symbol used to mark the board
-	 * @return boolean, true if square is empty, false if full, meaning can't be marked
+	 * @return boolean, true if square is empty, false if full (can't be marked).
 	 * @see GameBoard#isValid(int) isValid()
 	 */
 	public boolean markSquare(int coord, char mark){
@@ -53,94 +53,6 @@ public class GameBoard {
 		return board[coord/10][coord%10] == EMPTY && 0 <= coord && coord <= 55 ;
 	}
 
-	/**
-	 * Determines if someone has won.
-	 * 
-	 * @see GameBoard#diagonalWin() diagonalWin()
-	 * @see GameBoard#horizontalWin(char[][] table) horizontalWin()
-	 * @see GameBoard#verticalWin(char[][] table) verticalWin()
-	 * @return boolean, true if someone has won, false otherwise
-	 */
-	public boolean hasWon() {
-		return (diagonalWin() || horizontalWin(board) || verticalWin(board));
-	}
-
-	/**
-	 * Determines if any row contains a winning sequence of characters
-	 * 
-	 * @param table the table for which to check is someone has won
-	 * @return boolean, true if it someone has won, false otherwise
-	 */
-	private boolean horizontalWin(char[][] table) {
-		for (char[] row: table) {
-			short consec = 0;
-			char current = row[0];
-			
-			for (char c : row) {
-				// for every row count how many consecutive characters there are
-				// if target is reached, return true
-				if (c == current && current != EMPTY) consec++;
-				else {
-					current = c;
-					consec = 1;
-				}
-				if (consec >= consecutive) return true;
-			} // row
-		}
-		return false;
-	}
-
-	/**
-	 * Inverts the table <code>table</code><br>
-	 * <code>table[i][j]</code> becomes <code>table[j][i]</code>
-	 * 
-	 * @param table the table to invert
-	 * @return table, the inverted table
-	 */
-	private char[][] invertTable(char[][] table){
-		char[][] newTable = new char[boardSize][boardSize];
-		
-		for (int i=0; i<newTable.length; i++)
-			for (int j=0; j<newTable.length; j++)
-				newTable[i][j] = table[j][i];
-		return newTable;
-	}
-	
-	/**
-	 * Determines if any column contains a winning sequence of characters.<br>
-	 * Inverts the table using the {@link GameBoard#invertTable(char[][] table) invertTable} method
-	 * then uses the {@link GameBoard#horizontalWin(char[][] table) horizontalWin} method to return the result
-	 * 
-	 * @return boolean, true if someone has won, false otherwise
-	 */
-	private boolean verticalWin(char[][] table) {
-		return horizontalWin(invertTable(table));
-	}
-	
-	/**
-	 * spaghetti mess
-	 * 
-	 * @return boolean, true if someone has won, false otherwise
-	 */
-	private boolean diagonalWin() {
-		int[][] directions = {{1, -1}, {1, 1}};		//{1,0}, {0,1} to replace all other checks as well
-	    for (int[] d : directions) {
-	        int dx = d[0];
-	        int dy = d[1];
-	        for (int x = 0; x < board.length; x++) {
-	            for (int y = 0; y < board.length; y++) {
-	                int lastx = x + 2*dx;
-	                int lasty = y + 2*dy;
-	                if (0 <= lastx && lastx < board.length && 0 <= lasty && lasty < board.length) {
-	                    char w = board[x][y];
-	                    if (w != EMPTY && w == board[x+dx][y+dy] && w == board[lastx][lasty])
-	                    	return true;	// winner
-	                }
-	            }
-	        }
-	    }
-	    return false; // no winner
-	}
 	
 	/**
 	 * Clears the board; fills all squares with <code>EMPTY</code>
@@ -168,13 +80,13 @@ public class GameBoard {
 	}	
 	
 	/**
-	 * One glorious method to check if the board is has a win.<br>
+	 * Checks if the game is over.<br>
 	 * Supports custom <code>directions</code> (i.e. steps to take on the board).<br>
 	 * Supports any amount of <code>consecutive</code> squares.
 	 * 
-	 * @return boolean, true if someone has won, false otherwise
+	 * @return boolean, true if someone has won.
 	 */
-	private boolean win() {
+	public boolean hasWon() {
 		int[][] directions = {{1, 0}, {0, 1}, {1, -1}, {1, 1}};
 	    for (int[] direction : directions) {
 	    	// for every direction...
@@ -208,7 +120,7 @@ public class GameBoard {
 	}
 
 	/**
-	 * Returns the GameBoard's board
+	 * Returns the GameBoard in 2D array format
 	 * 
 	 * @return char[][], the board
 	 */
