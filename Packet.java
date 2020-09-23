14c378153e6b8ef39d38e5acb59317f0f766484a
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-09-23 12:40:40 +0300

    Added toString
    never used but why not :p might be useful later

--- Packet.java
+++ Packet.java
@@ -3,14 +3,19 @@ package ttt_online;
 import java.io.Serializable;
 
 public class Packet<T> implements Serializable {
-
+	
 	private static final long serialVersionUID = -2755557613017784530L;
 
-	public final String attribute;
+	public final int attribute;
 	public final T value;
 
-	public Packet(String attribute, T value) {
+	public Packet(int attribute, T value) {
 		this.attribute = attribute;
 		this.value = value;
+	}
+	
+	// never actually used but why not
+	public String toString() {
+		return String.format("Packet: [%d, %s]", attribute, value);
 	}
 }
