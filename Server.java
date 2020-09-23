bb9ca33eb2e67760a9dd7149eb9e9c7343ab85b7
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-09-23 14:26:20 +0300

    updated server
    idk what changed tbh

--- Server.java
+++ Server.java
@@ -12,9 +12,15 @@ import java.net.SocketException;
  * Server-side application to handle communications with the clients
  */
 public class Server {
+	private static final int GAME_PORT = 10001;
+	public static final int DATA = 0;
+	public static final int MOVE = 1;
+	public static final int TEXT = 2;
+	public static final int CHAT = 3;
+
 	private final int playerCount;
-	private boolean printStackTrace;
-	
+	private final boolean printStackTrace;
+
 	private ServerSocket server;
 	private final Socket[] sockets;
 	private final ObjectInputStream[] inputs;
@@ -22,26 +28,27 @@ public class Server {
 
 	private final char[] symbols;
 	private final Color[] colors;
-	
+
 	private final GameBoard gameBoard = new GameBoard();
 	private int currentPlayer = 0;
-	
+
 	/**
 	 * Constructor to initialise fields
 	 * 
-	 * @param playerCount int, the number of players
-	 * @param printStackTrace boolean, whether or not to print full Stack Trace when exceptions occur
+	 * @param playerCount     int, the number of players
+	 * @param printStackTrace boolean, whether or not to print full Stack Trace when
+	 *                        exceptions occur
 	 */
 	public Server(int playerCount, boolean printStackTrace) {
 		this.printStackTrace = printStackTrace;
 		this.playerCount = playerCount;
-		sockets = new Socket[playerCount];                        
-		inputs = new ObjectInputStream[playerCount];   
+		sockets = new Socket[playerCount];
+		inputs = new ObjectInputStream[playerCount];
 		outputs = new ObjectOutputStream[playerCount];
-		symbols = new char[playerCount];   
-		colors = new Color[playerCount];                            
+		symbols = new char[playerCount];
+		colors = new Color[playerCount];
 	}
-	
+
 	/**
 	 * Main method that calls other methods to actually run the server
 	 * 
@@ -52,7 +59,6 @@ public class Server {
 	private void run() {
 		initialiseServer();
 		getConnections();
-
 		log("Starting game");
 		while (true) {
 			makeTurn();
@@ -60,51 +66,52 @@ public class Server {
 	}
 
 	/**
-	 * Initialises the server on port 12345 with <code>playerCount</code> of connections.
-	 * 
+	 * Initialises the server on port <code>GAME_PORT</code> with <code>playerCount</code> connections.
 	 */
 	private void initialiseServer() {
 		try {
-			server = new ServerSocket(12345, playerCount);
+			server = new ServerSocket(GAME_PORT, playerCount);
 			log("\n\nServer ready, waiting for %d players", playerCount);
-		} 
-		catch (IOException e) {
+		} catch (IOException e) {
 			logerr("Error while setting up server");
 			if (printStackTrace) e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Initialises <code>playerCount</code> connections.<br>
 	 * Gets their input and output streams.<br>
-	 * Exchanges some messages.<br><br>
+	 * Exchanges some messages.<br>
+	 * <br>
 	 * If at any point something goes wrong, reset server :)
 	 */
+	@SuppressWarnings("unchecked")
 	private void getConnections() {
 		boolean reset = false;
 		try {
 			// connect to every player
-			for (int i=0; i<playerCount; i++) {
+			for (int i = 0; i < playerCount; i++) {
 				// get connections
 				sockets[i] = server.accept();
 				inputs[i] = new ObjectInputStream(sockets[i].getInputStream());
 				outputs[i] = new ObjectOutputStream(sockets[i].getOutputStream());
-				
+
 				// exchange send ack message
-				outputs[i].writeObject(String.format("Hi player #%d, you're now connected.\nPlease wait for others to join\n", i));
+				outputs[i].writeObject(new Packet<String>(TEXT,
+						String.format("Hi player #%d, you're now connected.\nPlease wait for others to join\n", i)));
 
 				// get player symbol
-				symbols[i] = (char) inputs[i].readObject();
-				colors[i] = (Color) inputs[i].readObject();
+				symbols[i] = ((Packet<Character>) inputs[i].readObject()).value;
+				colors[i] = ((Packet<Color>) inputs[i].readObject()).value;
 
 				log("Player #%d connected", i);
 			}
-			
+
 			// send ready message and symbol and color array
-			for (int j=0; j<playerCount; j++) {
-				outputs[j].writeObject("Everyone has joined; get ready to start the game!");
-				outputs[j].writeObject(symbols);
-				outputs[j].writeObject(colors);
+			for (int j = 0; j < playerCount; j++) {
+				outputs[j].writeObject(new Packet<String>(TEXT, "Everyone has joined; get ready to start the game!"));
+				outputs[j].writeObject(new Packet<char[]>(DATA, symbols));
+				outputs[j].writeObject(new Packet<Color[]>(DATA, colors));
 			}
 
 		} catch (IOException e) {
@@ -116,7 +123,7 @@ public class Server {
 			if (printStackTrace) e.printStackTrace();
 			reset = true;
 		}
-		
+
 		if (reset) {
 			reset();
 			run();
@@ -132,6 +139,7 @@ public class Server {
 	 * <li>Sends acknowledgement for the move
 	 * <li>Resends board
 	 */
+	@SuppressWarnings({ "rawtypes" })
 	private void makeTurn() {
 		boolean reset = false;
 		log("\nPlayer #%d starts their turn", currentPlayer);
@@ -139,23 +147,31 @@ public class Server {
 			log("Sent board:\n%s", gameBoard);
 			int move;
 			String response;
-			
+
 			// send ok to start
-			outputs[currentPlayer].writeObject("Make your move!");
+			outputs[currentPlayer].writeObject(new Packet<String>(TEXT, "Make your move!"));
 
 			// send board
 			sendBoard(currentPlayer);
-		
+
 			// get, register and respond to move
-			move = (int) inputs[currentPlayer].readObject();
-		
+			Packet p = (Packet) inputs[currentPlayer].readObject();
+			log("Just got '%s'", p.value);
+			if (p.attribute == Server.MOVE) {
+				move = (int) p.value;
+			} else {
+				logerr("Server received wrong packet; please close and inform the developers");
+				while (true) {;}
+			}
+
 			if (move == -2) {
 				log("Final board:\n%s", gameBoard);
-				broadcast("Player '%c' resigned", symbols[currentPlayer]);				
+				broadcast("Player '%c' resigned", symbols[currentPlayer]);
 				log("Player '%c' resigned!\nGame over", symbols[currentPlayer]);
-				for (int i=0; i<playerCount; i++) {
+				for (int i = 0; i < playerCount; i++) {
 					sendBoard(i);
 				}
+
 				log("Server will now reset");
 				reset();
 				run();
@@ -166,23 +182,24 @@ public class Server {
 				log("Final board:\n%s", gameBoard);
 				broadcast("Player '%c' won!", symbols[currentPlayer]);
 				log("Player '%c' won!\nGame over", symbols[currentPlayer]);
-				for (int i=0; i<playerCount; i++) {
+				for (int i = 0; i < playerCount; i++) {
 					sendBoard(i);
 				}
+
 				log("Server will now reset");
 				reset();
 				run();
 			}
-			
-			//send response to move
-			response = (String.format("Move received: [%c, %d]", 65+move/10, move%10+1));
-			outputs[currentPlayer].writeObject(new String(response));
-			
+
+			// send response to move
+			response = (String.format("Move received: [%c, %d]", 65 + move / 10, move % 10 + 1));
+			outputs[currentPlayer].writeObject(new Packet<String>(TEXT, response));
+
 			log("Move received: %d, response sent %s", move, response);
-		
-			//send board again
+
+			// send board again
 			sendBoard(currentPlayer);
-			
+
 			currentPlayer = (currentPlayer + 1) % playerCount;
 
 		} catch (SocketException e) {
@@ -204,7 +221,7 @@ public class Server {
 			run();
 		}
 	}
-	
+
 	/**
 	 * Resets everything in case something goes wrong while getting connections.<br>
 	 * Closes connections and empties <code>symbols</code> array.
@@ -217,7 +234,7 @@ public class Server {
 			logerr("IOException while closing server while reset");
 			e1.printStackTrace();
 		}
-		for (int i=0; i<playerCount; i++) {
+		for (int i = 0; i < playerCount; i++) {
 			try {
 				sockets[i].close();
 				inputs[i].close();
@@ -228,46 +245,52 @@ public class Server {
 				logerr("Error while resetting %d; player disconnected\n", i);
 			}
 		}
+
 		log("Done resetting");
 	}
-	
+
 	/**
 	 * Sends the board to the currentPlayer.<br>
 	 * Creates a copy of the GameBoard's board,<br>
 	 * and sends that copy because Java (:
-	 * @throws SocketException Thrown if client disconnects while trying to send board
+	 * 
+	 * @throws SocketException Thrown if client disconnects while trying to send
+	 *                         board
 	 */
 	private void sendBoard(int currentPlayer) throws SocketException {
 		char[][] newBoard = new char[5][5];
 		char[][] currentBoard = gameBoard.getBoard();
-		for (int i=0; i<5; i++)
-			for (int j=0; j<5; j++) 
+		for (int i = 0; i < 5; i++)
+			for (int j = 0; j < 5; j++)
 				newBoard[i][j] = currentBoard[i][j];
-		try {outputs[currentPlayer].writeObject(newBoard);}
-		catch (IOException e) {
+		try {
+			outputs[currentPlayer].writeObject(new Packet<char[][]>(DATA, newBoard));
+		} catch (IOException e) {
 			if (printStackTrace) e.printStackTrace();
 		}
 	}
-	
+
 	/**
-	 * Sends message <code>msg</code> to every client connected<br><code>System.out.printf(text, args)</code>
+	 * Sends message <code>msg</code> to every client connected<br>
+	 * <code>System.out.printf(text, args)</code>
 	 * 
-	 * 
-	 * @param text String, text to send
-	 * @param args Object[], arguments
+	 * @param boolean isServer, whether the message was sent by the server
+	 * @param text    String, text to send
+	 * @param args    Object[], arguments
 	 */
 	private void broadcast(String msg, Object... args) {
-		for (int i=0; i<playerCount; i++) {
+		for (int i = 0; i < playerCount; i++) {
 			try {
-				outputs[i].writeObject(String.format(msg, args));
+				outputs[i].writeObject(new Packet<String>(TEXT, String.format(msg, args)));
 			} catch (IOException e) {
 				logerr("Error while broadcasting");
 				if (printStackTrace) e.printStackTrace();
 			}
 		}
+		
 		log("Broadcasted: %s", String.format(msg, args));
 	}
-	
+
 	/**
 	 * Same as <code>System.out.printf(text, args)</code>
 	 * 
@@ -275,9 +298,9 @@ public class Server {
 	 * @param args Object[], arguments
 	 */
 	private static void log(String text, Object... args) {
-		System.out.printf(text+"\n", args);
+		System.out.printf(text + "\n", args);
 	}
-	
+
 	/**
 	 * Same as <code>System.err.printf(text, args)</code>
 	 * 
@@ -285,31 +308,37 @@ public class Server {
 	 * @param args Object[], arguments
 	 */
 	private static void logerr(String text, Object... args) {
-		System.err.printf(text+"\n", args);
+		System.err.printf(text + "\n", args);
 	}
 
 	/**
 	 * Main method. Run to create and run a server
 	 * 
 	 * @param args args[0] is used for the number of players expected to connect
-	 * @param args args[1] is used to determine <code>printStackTrace</code> field, '1' for true, other for false
+	 * @param args args[1] is used to determine <code>printStackTrace</code> field,
+	 *             '1' for true, other for false
 	 */
 	public static void main(String[] args) {
 		int playerCount;
-		try {playerCount = Integer.parseInt(args[0]);}
-		catch (ArrayIndexOutOfBoundsException e) {
+		boolean printStackTrace;
+
+		try {
+			playerCount = Integer.parseInt(args[0]);
+		} catch (ArrayIndexOutOfBoundsException e) {
 			logerr("Warning: No <player_count> argument provided;\nInitialised to 2");
 			playerCount = 2;
 		} catch (NumberFormatException e) {
 			logerr("Warning: <player_count> argument has illegal format;\nInitialised to 2");
 			playerCount = 2;
 		}
-		boolean printStackTrace;
-		try {printStackTrace = args[1].equals("1") ? true : false;}
-		catch (ArrayIndexOutOfBoundsException e) {
+
+		try {
+			printStackTrace = args[1].equals("1") ? true : false;
+		} catch (ArrayIndexOutOfBoundsException e) {
 			logerr("Warning: No <printStackTrace> argument provided;\nInitialised to false;");
 			printStackTrace = false;
 		}
+
 		Server server = new Server(playerCount, printStackTrace);
 		server.run();
 	}
