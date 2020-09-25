d11f756ed2f4d1d7509acd64054d112992f29421
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-09-25 19:17:49 +0300

    Update GameUI.java

--- GameUI.java
+++ GameUI.java
@@ -13,176 +13,238 @@ import javax.swing.*;
 import javax.swing.text.DefaultCaret;
 
 @SuppressWarnings("serial")
-public class GameUI extends JFrame{
+public class GameUI extends JFrame {
 	/*
-	 * Use getSymbol, getColor to get the char/color of each UI
-	 * Then use the public method setCustomOptions by giving a char and color array such that ```char of player i -> color of player i```
-	 * In any other case the game will use a blue X and a red O for the players
+	 * Use getSymbol, getColor to get the char/color of each UI. Then use the public
+	 * method setCustomOptions by giving a char and color array such that ```char of
+	 * player i -> color of player i```. In any other case the game will use a blue X
+	 * and a red O for the players
 	 */
-	
+
 	private char name;
 	private Color color;
-	private String chatText="";
-	private boolean dataReceived = true;
+	private String chatText = "";
+	private boolean dataReceived = false;
 	private int answer = -1;
 	private final JLabel error_msg;
 	private final Screen screen;
 	private final JTextArea log;
-	private final JTextField player,move,chatField;
-	private final JButton submitB,disconnectB,chatButton;
-	private final JPanel autismPanel,inputPanel,chatPanel,logPanel;
+	private final JTextField player, move, chatField;
+	private final JButton submitB, disconnectB, chatButton;
+	private final JPanel autismPanel, inputPanel, chatPanel, logPanel;
 	private final JScrollPane scroll;
-	
-	
+
+	public static void main(String[] args) {
+		GameUI ui = new GameUI();
+		ui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ui.setSize(320, 720);
+		ui.setVisible(true);
+		ui.setResizable(true);
+	}
+
 	public GameUI() {
 		super("Naughts & Crosses Online");
-		
-		getOptions();		
-		setLayout(new BoxLayout(getContentPane(),BoxLayout.PAGE_AXIS));
-		screen = new Screen();
-		screen.setPreferredSize(new Dimension(500,500));
-		
-		logPanel = new JPanel();
-		logPanel.setLayout(new BoxLayout(logPanel,BoxLayout.Y_AXIS));
-		chatPanel = new JPanel();
-		chatPanel.setLayout(new BoxLayout(chatPanel,BoxLayout.X_AXIS));
-		chatButton = new JButton("Send");
-		chatField = new JTextField();
-		chatButton.addActionListener(new ActionListener() {
 
+		getOptions();
+		setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
+
+		// screen
+		screen = new Screen();
+		screen.setPreferredSize(new Dimension(500, 500));
+
+		// logPanel
+		logPanel = new JPanel();
+		logPanel.setLayout(new BoxLayout(logPanel, BoxLayout.Y_AXIS));
+
+		// logPanel -- scroll -- log
+		log = new JTextArea("This is a message log\n");
+		log.setEditable(false);
+		// scroll to the bottom when new messages are pushed
+		((DefaultCaret) log.getCaret()).setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+
+		scroll = new JScrollPane(log, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scroll.setPreferredSize(new Dimension(100, 250));
+
+/*ADD*/ logPanel.add(scroll);
+
+		// logPanel -- chatPanel
+		chatPanel = new JPanel();
+		chatPanel.setLayout(new BoxLayout(chatPanel, BoxLayout.X_AXIS));
+
+		// logPanel -- chatPanel -- chatField / chatButton
+		chatField = new JTextField();
+		chatButton = new JButton("Send");
+		chatButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				chatText += (chatText.equals("")? "":"\n") + chatField.getText();
+				chatText += (chatText.equals("") ? "" : "\n") + chatField.getText();
 				chatField.setText("");
-				System.out.println(chatText);
-				
 			}
-		
 		});
-		
-		log = new JTextArea("This is a message log");
-		scroll = new JScrollPane (log,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		scroll.setPreferredSize(new Dimension(100,250));
-		log.setEditable(false);
-		chatPanel.add(chatField); chatPanel.add(chatButton);
-		logPanel.add(scroll); logPanel.add(chatPanel);
-		
-		autismPanel = new JPanel(); autismPanel.setLayout(new FlowLayout()); //because there is no other way to force Layout to cooperate
-		player = new JTextField("You are player "+name);
+
+/*AD */ chatPanel.add(chatField);
+/*ADD*/ chatPanel.add(chatButton);
+/*ADD*/ logPanel.add(chatPanel);
+
+		// autismPanel
+		autismPanel = new JPanel();
+		autismPanel.setLayout(new FlowLayout()); // because there is no other way to force Layout to cooperate
+
+		// autismPanel -- player
+		player = new JTextField("You are player " + name);
 		player.setEditable(false);
-		autismPanel.add(player);
-		
+
+/*ADD*/ autismPanel.add(player);
+
+		// error_msg
 		error_msg = new JLabel("Invalid input: Insert [letter][number]");
 		error_msg.setVisible(false);
-		
+
+		// inputPanel
 		inputPanel = new JPanel();
 		inputPanel.setLayout(new FlowLayout());
-		move = new JTextField("Your next move",10); inputPanel.add(move);
-		submitB = new JButton("Submit"); inputPanel.add(submitB); submitB.setMnemonic(KeyEvent.VK_SPACE);
+
+		// inputPanel -- move / submitB
+		move = new JTextField("Your next move", 10);
+		submitB = new JButton("Submit");
+		submitB.setMnemonic(KeyEvent.VK_SPACE);
 		submitB.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				if(!dataReceived) {
-					JOptionPane.showMessageDialog(getContentPane(), "Please wait for the other players to finish picking their characters","Error",JOptionPane.ERROR_MESSAGE);
+				if (!dataReceived) {
+					JOptionPane.showMessageDialog(getContentPane(),
+							"Please wait for the other players to finish picking their characters", "Error",
+							JOptionPane.ERROR_MESSAGE);
 					return;
 				}
-				
 				String input = move.getText().toUpperCase().strip();
 				if (!input.matches("[A-E][1-5]")) {
 					error_msg.setVisible(true);
 					return;
 				}
 				answer = convertInput(input);
-				pushMessage(String.format("Player %s played %s", name, move.getText()));
+				// un-comment if you need for debugging
+//				pushMessage(String.format("Player %s played %s", name, move.getText()));
 				move.setText("");
 				error_msg.setVisible(false);
 			}
 		});
-		
-		disconnectB = new JButton("Resign"); disconnectB.setMnemonic(KeyEvent.VK_R);
-		disconnectB.addActionListener(new ActionListener() {
+/*ADD*/ inputPanel.add(move);
+/*ADD*/ inputPanel.add(submitB);
 
+		// disconnectB
+		disconnectB = new JButton("Resign");
+		disconnectB.setMnemonic(KeyEvent.VK_R);
+		disconnectB.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				if(!dataReceived) {
-					JOptionPane.showMessageDialog(getContentPane(), "Please wait for the other players to finish picking their characters","Error",JOptionPane.ERROR_MESSAGE);
+				if (!dataReceived) {
+					JOptionPane.showMessageDialog(getContentPane(),
+							"Please wait for the other players to finish picking their characters", "Error",
+							JOptionPane.ERROR_MESSAGE);
 					return;
 				}
 				answer = -2;
-				pushMessage("Player "+ name +" resigned the game!");
+				// un-comment if you need for debugging
+//				pushMessage("Player "+ name +" resigned the game!");
 			}
-			
 		});
-		
-		add(Box.createRigidArea(new Dimension(50,35)));
+
+/* ADD TO JFRAME */
+		add(Box.createRigidArea(new Dimension(50, 35)));
 		add(autismPanel);
 		add(screen);
-		add(Box.createRigidArea(new Dimension(20,15)));
+		add(Box.createRigidArea(new Dimension(20, 15)));
 		add(inputPanel);
 		add(error_msg);
-		add(Box.createRigidArea(new Dimension(50,15)));
+		add(Box.createRigidArea(new Dimension(50, 15)));
 		add(logPanel);
-		add(Box.createRigidArea(new Dimension(50,35)));
+		add(Box.createRigidArea(new Dimension(50, 35)));
 		add(disconnectB);
-		pushMessage("Waiting for all other players to choose a character");
-	}
-	
-	public void pushMessage(String mes) {
-		log.setText(String.format("%s\n%s", log.getText(), mes));
-		
-		// scroll to bottom
-		
 	}
 
+	public void pushMessage(String mes) {
+		log.setText(String.format("%s%s\n", log.getText(), mes));
+	}
 	
+	// for future use so we can do:
+	// ui.pushMessage("player %s played %d", ui.getSymbol, move);
+	// instead of:
+	// ui.pushMessage(String.format("player %s played %d", ui.getSymbol, move));
+	// similar to how GameEngine#log works
+	public void pushMessage(String mes, Object... args) {
+		log.setText(String.format("%s%s\n", log.getText(), mes, args));
+	}
+
+	public void pushMessage(String mes, boolean newline) {
+		log.setText(String.format("%s%s%s", log.getText(), mes, newline ? "\n" : ""));
+	}
+
 	public void setScreen(GameBoard gboard) {
 		screen.board = gboard;
 		screen.repaint();
-		//Wait until it loads then update the whole thing.
-		//JVM has forced my hand
+		// Wait until it loads then update the whole thing.
+		// JVM has forced my hand
+		// spaghetti
 		try {
-			Thread.sleep(100);  
-		}catch (InterruptedException e){
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+
 		this.update(this.getGraphics());
 	}
-	
+
 	public void setEnableTurn(boolean enable) {
 		submitB.setEnabled(enable);
 		disconnectB.setEnabled(enable);
 		move.setEnabled(enable);
+		if (!enable)
+			move.setText("Your next move");
+		else
+			move.setText("");
 	}
-	
+
+	public void setEnableChat(boolean enable) {
+		chatButton.setEnabled(enable);
+		chatField.setEnabled(enable);
+		if (!enable)
+			chatField.setText("You're not connected to chat server");
+		else
+			chatField.setText("");
+	}
+
 	public int getAnswer() {
 		int ans = -1;
-		if (this.answer != -1) { 
+		if (this.answer != -1) {
 			ans = this.answer;
 			this.answer = -1;
-		} 
+		}
 		return ans;
 	}
-		
+
 	public char getSymbol() {
 		return this.name;
 	}
 
-	
 	public Color getColor() {
 		return this.color;
 	}
-	
+
+	// same logic as getAnswer()
 	public String getChatText() {
-		String temp = this.chatText;
-		chatText = "";
-		return temp;
+		String ans = "";
+		if (!this.chatText.equals("")) {
+			ans = this.chatText;
+			this.chatText = "";
+		}
+		return ans;
 	}
-	
+
 	/**
-	 * Converts the player's input from <code>Char-Int</code> to <code>Int-Int</code>
-	 * so that the GameBoard can understand it
+	 * Converts the player's input from <code>Char-Int</code> to
+	 * <code>Int-Int</code> so that the GameBoard can understand it
 	 * 
 	 * @param str String, the player's input
 	 * @return int, the data the GameBoard understands
@@ -191,98 +253,104 @@ public class GameUI extends JFrame{
 		char row = str.charAt(0);
 		int index;
 		switch (row) {
-			case '\u0041':
-				index = 0; 
-				break;
+		case '\u0041':
+			index = 0;
+			break;
 
-			case '\u0042':
-				index = 10;
-				break;
+		case '\u0042':
+			index = 10;
+			break;
 
-			case '\u0043':
-				index = 20;
-				break;
+		case '\u0043':
+			index = 20;
+			break;
 
-			case '\u0044':
-				index = 30;
-				break;
-			
-			default: index = 40;
+		case '\u0044':
+			index = 30;
+			break;
+
+		default:
+			index = 40;
 		}
-		index += Integer.parseInt(Character.toString(str.charAt(1)))-1;
+
+		index += Integer.parseInt(Character.toString(str.charAt(1))) - 1;
 		return index;
 	}
-	
-	private void getOptions() { //gets character player and color
-		
-		Object [] chars = {GameEngine.X,GameEngine.O,'\u2654' ,'\u2655' ,'\u2656' ,'\u2657' ,'\u2658','\u0021' ,'\u0022' ,'\u0023' ,'\u0024' ,'\u0025' ,'\u002A','\u002B','\u0041' ,'\u0042' ,'\u0043' ,'\u0044' ,'\u0045' ,'\u0046' ,'\u0047' ,'\u0048' ,'\u0049' ,'\u0050' ,'\u0051' 
-				,'\u0052' ,'\u0053' ,'\u0054' ,'\u0055' ,'\u0056' ,'\u0057' ,'\u003C','\u003F','\u007E'};
-		
-		boolean correct;
+
+	// gets character player and color
+	private void getOptions() {
+
+		Object[] chars = { GameEngine.X, GameEngine.O, '\u0021', '\u0022', '\u0023', '\u0024', '\u0025', '\u002A',
+				'\u002B', '\u0041', '\u0042', '\u0043', '\u0044', '\u0045', '\u0046', '\u0047', '\u0048', '\u0049',
+				'\u0050', '\u0051', '\u0052', '\u0053', '\u0054', '\u0055', '\u0056', '\u0057', '\u003C', '\u003F',
+				'\u007E' };
+
+		boolean correct = false;
 		do {
 			try {
-				this.name = (Character)JOptionPane.showInputDialog(null,"Select one of the following characters","Character Selection",JOptionPane.PLAIN_MESSAGE,null,chars,chars[0]);
+				this.name = (Character) JOptionPane.showInputDialog(null, "Select one of the following characters",
+						"Character Selection", JOptionPane.PLAIN_MESSAGE, null, chars, chars[0]);
 				correct = true;
-			}catch (NullPointerException e) {
+			} catch (NullPointerException e) {
 				JOptionPane.showMessageDialog(this, "Please select a character", "Error", JOptionPane.ERROR_MESSAGE);
 				correct = false;
 			}
-		}while(!correct);
-		
+		} while (!correct);
+
 		Color col;
 		do {
 			col = JColorChooser.showDialog(this, "Choose a color", Color.BLACK);
-			if (col==null) JOptionPane.showMessageDialog(this, "Please select a color", "Error", JOptionPane.ERROR_MESSAGE);
-		}while(col==null);
+			if (col == null)
+				JOptionPane.showMessageDialog(this, "Please select a color", "Error", JOptionPane.ERROR_MESSAGE);
+		} while (col == null);
+
 		this.color = col;
-		
 	}
-	
+
 	public void setCustomOptions(char[] chars, Color[] colors) {
-		if(colors.length!=chars.length) throw new RuntimeException("Color and character arrays must be of same length");
-		
-		for(int i=0;i<colors.length;i++) {
+		if (colors.length != chars.length)
+			throw new RuntimeException("Color and character arrays must be of same length");
+
+		for (int i = 0; i < colors.length; i++)
 			screen.colorMap.put(chars[i], colors[i]);
-		}
+
 		dataReceived = true;
 	}
-	
 
-	private class Screen extends JPanel{
+	private class Screen extends JPanel {
 		private GameBoard board;
-		private final HashMap<Character,Color> colorMap = new HashMap<Character,Color>();
-		private String[] letters = {"A","B","C","D","E"};
-		
+		private final HashMap<Character, Color> colorMap = new HashMap<Character, Color>();
+		private String[] letters = { "A", "B", "C", "D", "E" };
+
 		public Screen() {
 			board = new GameBoard();
 		}
-		
-		
+
 		@Override
 		public void paintComponent(Graphics g) {
 			g.setColor(Color.BLACK);
 			g.setFont(new Font("Serif", Font.PLAIN, 60));
-			//paint board
-			for(int i=1; i<6; i++) {
-				g.drawString(Integer.toString(i), i*53, 50);//53 instead of 50 to compensate for character width
-				for(int j=1;j<6;j++) {
-					g.drawString(letters[j-1], 0, (j+1)*50);
-					g.drawRect(i*50, j*50, 50, 50);
+
+			// paint board
+			for (int i = 1; i < 6; i++) {
+				g.drawString(Integer.toString(i), i * 53, 50);// 53 instead of 50 to compensate for character width
+				for (int j = 1; j < 6; j++) {
+					g.drawString(letters[j - 1], 0, (j + 1) * 50);
+					g.drawRect(i * 50, j * 50, 50, 50);
 				}
 			}
-			//paint marks
+
+			// paint marks
 			g.setFont(new Font("Monospaced", Font.BOLD, 65));
-			for(int i=0;i<5;i++) {
-				for(int j=0;j<5;j++) {
+			for (int i = 0; i < 5; i++) {
+				for (int j = 0; j < 5; j++) {
 					char c = board.getBoard()[i][j];
-					if (c!=GameEngine.DASH) { 
+					if (c != GameEngine.DASH) {
 						g.setColor(colorMap.get(c));
-						g.drawString(Character.toString(c), (j+1)*50, (i+2)*50);
-					
+						g.drawString(Character.toString(c), (j + 1) * 50, (i + 2) * 50);
 					}
 				}
 			}
-		}		
+		}
 	}
-	
 }
