eb684bbcccfe34914a6e4ec031689e37e6b7a198
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-09-25 12:24:04 +0300

    added format_project
    creates src\ttt_online, bin, scripts
    puts the code in src\ttt_online
    creates compile.bat, run_server_game.bat, run_server_game.bat, run_client.bat
    
    TODO: remove last 'rem' to delete this file after execution

---
+++ format_project.bat
@@ -0,0 +1,17 @@
+rem create directories
+mkdir src\ttt_online
+mkdir bin
+mkdir scripts
+move *.java src\ttt_online
+
+rem create .bat files
+echo javac -d ..\bin  --release 14 ..\src\ttt_online\*.java > scripts\compile.bat
+echo java -cp ..\bin ttt_online.GameEngine > scripts\run_client.bat
+echo java -cp ..\bin ttt_online.Server > scripts\run_server_game.bat
+echo java -cp ..\bin ttt_online.ChatServer > scripts\run_server_chat.bat
+
+rem create TODO file
+echo find the Java version by running `java -version` and replace that number in the scripts\compile.bat --release flag > TODO.txt
+
+rem delete this file
+rem del format_project.bat
