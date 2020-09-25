cb7895b99bc6b9a231d803e4a7341f055c159042
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-09-25 19:19:04 +0300

    fixed typo
    Server renamed to GameServer

--- format_project.bat
+++ format_project.bat
@@ -7,7 +7,7 @@ move *.java src\ttt_online
 rem create .bat files
 echo javac -d ..\bin  --release 14 ..\src\ttt_online\*.java > scripts\compile.bat
 echo java -cp ..\bin ttt_online.GameEngine > scripts\run_client.bat
-echo java -cp ..\bin ttt_online.Server > scripts\run_server_game.bat
+echo java -cp ..\bin ttt_online.GameServer > scripts\run_server_game.bat
 echo java -cp ..\bin ttt_online.ChatServer > scripts\run_server_chat.bat
 
 rem create TODO file
