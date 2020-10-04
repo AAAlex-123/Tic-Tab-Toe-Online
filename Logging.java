4f1967b242fbe32ee5f2fe933ffc93d4b4c2f880
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-10-04 15:37:10 +0300

    Update Logging.java

--- Logging.java
+++ Logging.java
@@ -1,8 +1,12 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 package ttt_online;
 
 /**
  * An interface for <code>log</code> and <code>logerr</code> methods providing an easily changeable print and error stream.
- * Default methods implement the System.out.printf and System.err.printf streams. 
  *
  */
 public interface Logging {
@@ -13,17 +17,29 @@ public interface Logging {
 	 * @param text String, text to send
 	 * @param args Object[], arguments
 	 */
-	default void log(String text, Object... args) {
-		System.out.printf(text + "\n", args);
+	default void log(String text) {
+		System.out.println(text);
 	}
 
 	/**
-	 * Same as <code>System.err.printf(text, args)</code>
+	 * Prints the error message to an external "error_log.txt" file.
+	 * Includes information about the date/time and class where the error occurred.
 	 * 
-	 * @param text String, text to send
-	 * @param args Object[], arguments
+	 * @param error_msg String, the line that will be printed on the file
 	 */
-	default void logerr(String text, Object... args) {
-		System.err.printf(text + "\n", args);
+	default void logerr(String error_msg) {
+		BufferedWriter writer;
+		LocalDateTime now = LocalDateTime.now();
+		String dateTime = String.format("%d/%d/%d %d:%d:%d",now.getDayOfMonth(),now.getMonthValue(),now.getYear(),now.getHour(),now.getMinute(),now.getSecond());
+		try {
+			writer = new BufferedWriter(new FileWriter("error_log.txt"));
+			writer.write(String.format("Crash at %s\nClass %s\nError:%s\n#########################################\n",dateTime,this.getClass().getName(),error_msg));
+			writer.close();
+		}catch (IOException e) {
+			System.err.println(e);
+			return;
+		}
+		
 	}
+	
 }
