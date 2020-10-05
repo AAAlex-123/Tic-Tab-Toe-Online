3953edbc8cd78988f861dff9a2d2c3d76d0ad1f2
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-10-05 19:08:56 +0300

    Update Logging.java
    updated loggerr
    
    kill me

--- Logging.java
+++ Logging.java
@@ -1,12 +1,17 @@
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.time.LocalDateTime;
+import java.util.Scanner;
 
-package ttt_online;
+//package ttt_online;
 
 /**
  * An interface for <code>log</code> and <code>logerr</code> methods providing an easily changeable print and error stream.
+ * Default methods implement the System.out.println and System.err.println streams. 
  *
  */
 public interface Logging {
@@ -25,21 +30,52 @@ public interface Logging {
 	 * Prints the error message to an external "error_log.txt" file.
 	 * Includes information about the date/time and class where the error occurred.
 	 * 
-	 * @param error_msg String, the line that will be printed on the file
+	 * @param error_msg String, a summary of the error
+	 * @param e Exception
+	 * @param printFull, whether or not to print the full error message
 	 */
-	default void logerr(String error_msg) {
-		BufferedWriter writer;
-		LocalDateTime now = LocalDateTime.now();
-		String dateTime = String.format("%d/%d/%d %d:%d:%d",now.getDayOfMonth(),now.getMonthValue(),now.getYear(),now.getHour(),now.getMinute(),now.getSecond());
+	default void logerr(String error_msg, Exception e, boolean printFull) {
+		
+		File file = new File("error_log.txt");
 		try {
-			writer = new BufferedWriter(new FileWriter("error_log.txt"));
-			writer.write(String.format("Crash at %s\nClass %s\nError:%s\n#########################################\n",dateTime,this.getClass().getName(),error_msg));
-			writer.close();
-		}catch (IOException e) {
-			System.err.println(e);
+			if (!file.exists()) file.createNewFile();
+		}catch(IOException ex) {
+			ex.printStackTrace();
 			return;
 		}
 		
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		e.printStackTrace(pw);
+		
+		synchronized(file){
+			Scanner in;
+			StringBuffer old_data = new StringBuffer("");
+			try {
+				 in = new Scanner(file);
+			}catch(IOException ex) {
+				ex.printStackTrace();
+				return;
+			}
+			while (in.hasNext()) {
+				old_data.append(in.nextLine()+"\n");
+			}
+			in.close();
+			
+			BufferedWriter writer;
+			LocalDateTime now = LocalDateTime.now();
+			String dateTime = String.format("%d/%d/%d %d:%d:%d",now.getDayOfMonth(),now.getMonthValue(),now.getYear(),now.getHour(),now.getMinute(),now.getSecond());
+			try {
+				writer = new BufferedWriter(new FileWriter("error_log.txt"));
+				writer.write(old_data+String.format("Crash at %s\nClass %s\nSummary:%s %s\n#########################################\n",dateTime,this.getClass().getName(),
+						error_msg, printFull? "Full report:\n"+sw.toString() :""));
+				writer.close();
+			}catch (IOException ex) {
+				System.err.println(ex);
+				return;
+			}
+		}
 	}
+
 	
 }
