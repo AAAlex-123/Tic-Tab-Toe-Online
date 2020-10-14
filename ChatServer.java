160118aef22b002df4adb005f1cf75f85ed60eed
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-10-14 14:34:38 +0300

    tiny fix
    when chatserver is run alone, it sets up screen

--- ChatServer.java
+++ ChatServer.java
@@ -52,7 +52,7 @@ final class ChatServer extends Server {
 	 */
 	ChatServer(int playerCount, boolean printStackTrace) {
 		super(playerCount, printStackTrace);
-		System.out.println(this.playerCount);
+		// System.out.println(this.playerCount);
 		this.available = new boolean[playerCount];
 		for (int i = 0; i < playerCount; i++)
 			available[i] = true;
@@ -281,6 +281,7 @@ final class ChatServer extends Server {
 	 */
 	public static void main(String[] args) {
 		ChatServer server = new ChatServer();
+		server.setupScreen();
 		server.run();
 	}
 }
